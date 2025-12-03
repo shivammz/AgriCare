8e65ab6c36307839e8e42bf0349b3cb925883755
shivammz <kishankkumar252525@gmail.com>
2025-12-03 18:54:56 +0530

    Delete server directory

--- server/api/__init__.py
+++
@@ -1,8 +0,0 @@
-# AgriCare/server/api/__init__.py
-
-from server.api.auth.signup import signup_router
-from server.api.auth.login import login_router
-from server.api.job import job_router
-from server.api.service import service_router
-
-__all__ = ["signup_router", "login_router", "job_router", "service_router"]
--- server/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# AgriCare/server/models/__init__.py
-
-from server.models.user import User
-from server.models.farmer import Farmer
-from server.models.labour import Labour
-from server.models.job import Job
-from server.models.service import Service
-
